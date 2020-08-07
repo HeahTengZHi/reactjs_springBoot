94e65ea6d0b0b2fb27471a94897f33c820332cfe
TengZhi <optimum.dev5@gmail.com>
2020-08-07 18:58:56 +0800

    dsa2

--- book-rest-api/src/main/java/com/reactspring/bookrestapi/BookApplication.java
+++ book-rest-api/src/main/java/com/reactspring/bookrestapi/BookApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookApplication {
 	
 	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
 	}
