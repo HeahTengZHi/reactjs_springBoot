60728f2c5d7f209eb958c5403ef2f9d4fc2946f3
TengZhi <optimum.dev5@gmail.com>
2020-08-16 14:04:34 +0800

    test

--- book-rest-api/src/main/java/com/reactspring/bookrestapi/BookApplication.java
+++ book-rest-api/src/main/java/com/reactspring/bookrestapi/BookApplication.java
@@ -7,7 +7,7 @@ public class BookApplication {
 	
 	
 	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
+		SpringApplication.run(BookApplication.class, args);dsadsad
 	}
 	
 }
